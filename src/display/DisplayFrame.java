c78471eef275305d526191c795b4f28362f352ba
Veronika Babic <s0554747@htw-berin.de>
2018-01-03 15:47:16 +0100

    project set up and update

---
+++ src/display/DisplayFrame.java
@@ -0,0 +1,142 @@
+package display;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.htw.fiw.vs.fernseher.IDisplayRemote;
+import org.htw.fiw.vs.fernseher.IServicePoint;
+
+public class DisplayFrame extends JFrame implements Runnable,ActionListener,IDisplayRemote{
+	private JPanel panel;
+	private JButton on = new JButton("Fernseher an");
+	private JButton off = new JButton("Fernseher aus");
+	boolean status = false;
+	public DisplayFrame displayFrame;
+	private ArrayList<IServicePoint> observers;
+	
+
+	public DisplayFrame() {
+		super("Display");
+		observers = new ArrayList<IServicePoint>();
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		
+
+		panel.add(on);
+		panel.add(off);
+		on.addActionListener(this);
+		off.addActionListener(this);
+
+		this.setSize(500, 500);
+		this.setVisible(true);
+		this.add(panel);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension screenSize = tk.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		setLocation(screenWidth / 8, screenHeight / 6);
+	}
+
+	
+	
+	
+
+
+	@Override
+	public void run() {
+
+		while(true){
+			try {
+				Thread.sleep(100);
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	Object ausloeser = e.getSource();
+		
+		if(ausloeser == on) {
+			try {
+				this.setStatus(true);
+			} catch (RemoteException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		if(ausloeser == off) {
+			try {
+				this.setStatus(false);
+			} catch (RemoteException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}	
+		}
+		
+		
+	}
+
+
+
+
+
+
+
+	@Override
+	public void register(IServicePoint newObserver) {
+		observers.add(newObserver);
+
+
+	}
+
+	@Override
+	public void unregister(IServicePoint deleteObserver) {
+		int observerIndex = observers.indexOf(deleteObserver);
+		System.out.println("Observer " + (observerIndex+1) + " deleted");
+
+		observers.remove(observerIndex);
+
+	}
+
+	@Override
+	public void notifyObserver() throws RemoteException {
+		for(IServicePoint observer : observers){    
+
+			observer.update(status);
+
+
+		}
+
+
+	}
+
+	public void setStatus(boolean newStatus) throws RemoteException{
+
+
+		this.status = newStatus;
+
+
+		notifyObserver();
+	}
+	
+}
---
+++ src/display/DisplayMain.java
@@ -0,0 +1,113 @@
+package display;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+import org.htw.fiw.vs.IBinder;
+import org.htw.fiw.vs.fernseher.IDisplayRemote;
+import org.htw.fiw.vs.fernseher.IServicePoint;
+
+public class DisplayMain extends UnicastRemoteObject implements IDisplayRemote{
+
+	private ArrayList<IServicePoint> observers;
+	private boolean status;
+
+
+	public DisplayFrame displayFrame;
+
+	public DisplayMain(DisplayFrame DisplayFrame) throws RemoteException {
+		super();
+		this.displayFrame = displayFrame;
+		observers = new ArrayList<IServicePoint>();
+
+
+	}
+
+	public static void main(String[] args) {
+
+		if(args.length == 2){
+
+			try {
+				String ip = args[0];
+				int port = Integer.parseInt(args[1]);
+				String protokoll = "rmi://";
+				String url = protokoll + ip + ":" + port + "/binder";
+				try {
+
+
+					DisplayFrame displayFrame = new DisplayFrame();
+					Thread displayFrameThread = new Thread(displayFrame);
+					displayFrameThread.start();
+					System.out.println("Display gestartet");
+
+					DisplayMain displayMain = new DisplayMain(displayFrame);
+					IBinder binder = (IBinder) Naming.lookup(url);
+					binder.bind("Display", displayMain);
+
+					System.out.println("Display-RMI-Binding war erfolgreich");
+
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} catch (NumberFormatException nfe) {
+				System.out.println("Kein gueltiger Port");
+			}
+
+		} else {
+			System.out.println("Bitte IP-Adresse und Port als Programmparameter uebergeben");
+		}
+	}
+
+
+	@Override
+	public void register(IServicePoint newObserver) {
+		observers.add(newObserver);
+
+
+	}
+
+	@Override
+	public void unregister(IServicePoint deleteObserver) {
+		int observerIndex = observers.indexOf(deleteObserver);
+		System.out.println("Observer " + (observerIndex+1) + " deleted");
+
+		observers.remove(observerIndex);
+
+	}
+
+	@Override
+	public void notifyObserver() throws RemoteException {
+		for(IServicePoint observer : observers){    
+
+			observer.update(status);
+
+
+		}
+
+
+	}
+
+	public void setStatus(boolean newStatus) throws RemoteException{
+
+
+		this.status = newStatus;
+
+
+		notifyObserver();
+	}
+
+}
